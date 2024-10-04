e2d6931f4ec3d4d7c1de1f5c34e094096580167f
Vivek-singh-23 <viveksinghy23@gmail.com>
2024-10-04 12:45:47 +0530

    Eighth commit

---
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -0,0 +1,54 @@
+"use client";
+import { PiggyBank, ReceiptText, Wallet } from "lucide-react";
+import React, { useEffect, useState } from "react";
+
+function CardInfo({ budgetList }) {
+    
+    const [totalBudget, setTotalBudget] = useState(0)    
+    const [totalSpend, setTotalSpend] = useState(0)    
+    console.log("CardInfo", budgetList)
+    useEffect(() => {
+      budgetList && CalculateCardInfo();
+    }, [budgetList])
+
+    const CalculateCardInfo=()=>{
+        let totalBudget_ = 0;
+        let totalSpend_ = 0;
+        budgetList.forEach(element=>{
+            totalBudget_=totalBudget_ + Number(element.amount)
+            totalSpend_ = totalSpend_ + element.totalSpend
+        })
+        console.log(totalBudget_, totalSpend_)
+        setTotalBudget(totalBudget_);
+        setTotalSpend(totalSpend_)
+    }
+  return (
+    <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      <div className="p-7 border rounded-lg flex items-center justify-between ">
+        <div className="">
+          <h2 className="text-sm">Total Budget</h2>
+          <h2 className="font-bold text-2xl">{totalBudget}</h2>
+        </div>
+        <PiggyBank className="bg-primary p-3 h-12 w-12  rounded-full text-white" />
+      </div>
+
+      <div className="p-7 border rounded-lg flex items-center justify-between ">
+        <div className="">
+          <h2 className="text-sm">Total Spend</h2>
+          <h2 className="font-bold text-2xl">{totalSpend}</h2>
+        </div>
+        <ReceiptText className="bg-primary p-3 h-12 w-12  rounded-full text-white" />
+      </div>
+
+      <div className="p-7 border rounded-lg flex items-center justify-between ">
+        <div className="">
+          <h2 className="text-sm">No. of Budgets</h2>
+          <h2 className="font-bold text-2xl">{budgetList?.length}</h2>
+        </div>
+        <Wallet className="bg-primary p-3 h-12 w-12  rounded-full text-white" />
+      </div>
+    </div>
+  );
+}
+
+export default CardInfo;
--- app/(routes)/dashboard/page.jsx
+++ app/(routes)/dashboard/page.jsx
@@ -1,12 +1,53 @@
-"use client"
-import React from 'react'
-
+"use client";
+import { useUser } from "@clerk/nextjs";
+import React, { useEffect, useState } from "react";
+import CardInfo from "./_components/CardInfo";
+import { db } from "@/utils/dbConfig";
+import { desc, eq, sql } from "drizzle-orm";
+import { Budgets, Expenses } from "@/utils/schema";
+import BarChartDashboard from "./_components/BarChartDashboard";
 
 function Dashboard() {
-    
+  const { user } = useUser();
+  const [budgetList, setBudgetList] = useState([]);
+
+  useEffect(() => {
+    user && getBudgetList();
+  }, [user]);
+
+  const getBudgetList = async () => {
+    try {
+      const result = await db
+        .select({
+          ...getTableColumns(Budgets),
+          totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number), // Sum of expenses
+          totalItem: sql`count(${Expenses.id})`.mapWith(Number), // Count of expense items
+        })
+        .from(Budgets)
+        .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+        .where(eq(Budgets.createdBy, user?.primaryEmailAddress?.emailAddress))
+        .groupBy(Budgets.id)
+        .orderBy(desc(Budgets.id));
+      setBudgetList(result);
+    } catch (error) {
+      console.error("Error fetching budget list:", error);
+    }
+  };
   return (
-    <div>Dashboard</div>
-  )
+    <div className="p-5">
+      <h2 className="font-bold text-3xl">Hi, {user?.fullName}</h2>
+      <p className="text-gray-500">
+        Here's what happening with your money, Let's manage your expense
+      </p>
+      <CardInfo budgetList={budgetList} />
+      <div className="grid grid-cols-1 md:grid-cols-3 mt-6">
+        <div className="md:col-span-2"><BarChartDashboard budgetList={budgetList}/></div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
